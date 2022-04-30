914ae2c755f6b154a467776397d4a3c2b5d1bfe1
Raúl Aguilar <ag21raul@gmail.com>
2022-04-30 12:31:33 +0200

    Creación del controlador para las animaciones

---
+++ core/src/es/iespolitecnicomalaga/spaceracer/ControladorAnimaciones.java
@@ -0,0 +1,47 @@
+package es.iespolitecnicomalaga.spaceracer;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
+import java.util.ArrayList;
+
+public class ControladorAnimaciones {
+    private ArrayList<Dibujable> frames;
+    private float numeroFrames;
+    private float tiempoCambioFrame;
+    private int indiceFrameActual;
+    private float tiempoFrame;
+
+    public ControladorAnimaciones(String carpeta, String extensionFrames, int numeroFrames, float tiempoCambioFrame) {
+        this.numeroFrames = numeroFrames;
+        this.tiempoCambioFrame = tiempoCambioFrame;
+        this.indiceFrameActual = 0;
+        this.tiempoFrame = 0;
+        frames = new ArrayList<Dibujable>();
+        for (int i = 0; i < numeroFrames; i++) {
+            Texture miTextura = new Texture(carpeta + "/" + i + "."+ extensionFrames);
+            frames.add(new DibujableAdaptador(miTextura));
+        }
+    }
+
+    public void pintarse(Lienzo miSB) {
+        miSB.draw(getFrameActual(), 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    }
+
+    private Dibujable getFrameActual() {
+
+        if (tiempoFrame < tiempoCambioFrame) {
+            tiempoFrame += Gdx.graphics.getDeltaTime();
+        } else {
+            tiempoFrame = 0;
+
+            if (indiceFrameActual >= numeroFrames - 1) {
+                indiceFrameActual = 0;
+            } else {
+                indiceFrameActual++;
+            }
+        }
+
+        return frames.get(indiceFrameActual);
+    }
+}
